811dcdd78437ae92c27a4524c98d1b377524dc79
tomhoon <gnsdl9108@naver.com>
2025-08-26 21:23:46 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MaterialApp(home: WebViewExample()));
+
+class WebViewExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebViewWidget(
+        controller: WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..loadRequest(Uri.parse("http://tomhoon.my:25000/swagger-ui/index.html")),
+      ),
+    );
+  }
+}
